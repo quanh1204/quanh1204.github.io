85ccbf91057744febf36cc847365b54e664cfa53
quanh1204 <129777203+quanh1204@users.noreply.github.com>
2024-01-18 14:55:03 +0000

    update

--- src/js/data.js
+++ src/js/data.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.,
+  apiKey: "AIzaSyDam-02kb9L8Eifer0W74zDA9DDjCQhndA",
+  authDomain: "muc-tieu.firebaseapp.com",
   projectId: "muc-tieu",
   storageBucket: "muc-tieu.appspot.com",
   messagingSenderId: "254030905097",
@@ -10,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-Z16QDLK4EP"
 };
 
+console.log("da chay");
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
